8ff2c070f97534b484fae8d4ac4445cc942530e1
dalexandrov <mitiaalexandrov@gmail.com>
2020-11-26 16:31:11 +0200

    CORS project added

---
+++ cors/src/main/java/io/helidon/kotlin/examples/cors/GreetService.kt
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2020 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.helidon.kotlin.examples.cors
+
+import io.helidon.common.http.Http
+import io.helidon.config.Config
+import io.helidon.kotlin.examples.cors.GreetingMessage.Companion.fromRest
+import io.helidon.webserver.*
+import javax.json.Json
+import javax.json.JsonObject
+
+/**
+ * A simple service to greet you. Examples:
+ *
+ * Get default greeting message:
+ * curl -X GET http://localhost:8080/greet
+ *
+ * Get greeting message for Joe:
+ * curl -X GET http://localhost:8080/greet/Joe
+ *
+ * Change greeting
+ * curl -X PUT -H "Content-Type: application/json" -d '{"greeting" : "Howdy"}' http://localhost:8080/greet/greeting
+ *
+ * The message is returned as a JSON object
+ */
+class GreetService internal constructor(config: Config) : Service {
+    /**
+     * The config value for the key `greeting`.
+     */
+    private var greeting: String
+
+    /**
+     * A service registers itself by updating the routine rules.
+     * @param rules the routing rules.
+     */
+    override fun update(rules: Routing.Rules) {
+        rules["/", Handler { request: ServerRequest, response: ServerResponse -> getDefaultMessageHandler(request, response) }]["/{name}", Handler { request: ServerRequest, response: ServerResponse -> getMessageHandler(request, response) }]
+                .put("/greeting", Handler { request: ServerRequest, response: ServerResponse -> updateGreetingHandler(request, response) })
+    }
+
+    /**
+     * Return a worldly greeting message.
+     * @param request the server request
+     * @param response the server response
+     */
+    private fun getDefaultMessageHandler(request: ServerRequest,
+                                         response: ServerResponse) {
+        sendResponse(response, "World")
+    }
+
+    /**
+     * Return a greeting message using the name that was provided.
+     * @param request the server request
+     * @param response the server response
+     */
+    private fun getMessageHandler(request: ServerRequest,
+                                  response: ServerResponse) {
+        val name = request.path().param("name")
+        sendResponse(response, name)
+    }
+
+    private fun sendResponse(response: ServerResponse, name: String) {
+        val msg = GreetingMessage(String.format("%s %s!", greeting, name))
+        response.send(msg.forRest())
+    }
+
+    private fun updateGreetingFromJson(jo: JsonObject, response: ServerResponse) {
+        if (!jo.containsKey(GreetingMessage.JSON_LABEL)) {
+            val jsonErrorObject = JSON_BF.createObjectBuilder()
+                    .add("error", "No greeting provided")
+                    .build()
+            response.status(Http.Status.BAD_REQUEST_400)
+                    .send(jsonErrorObject)
+            return
+        }
+        greeting = fromRest(jo).message
+        response.status(Http.Status.NO_CONTENT_204).send()
+    }
+
+    /**
+     * Set the greeting to use in future messages.
+     * @param request the server request
+     * @param response the server response
+     */
+    private fun updateGreetingHandler(request: ServerRequest,
+                                      response: ServerResponse) {
+        request.content().`as`(JsonObject::class.java).thenAccept { jo: JsonObject -> updateGreetingFromJson(jo, response) }
+    }
+
+    companion object {
+        private val JSON_BF = Json.createBuilderFactory(emptyMap<String, Any>())
+        private val JSON_RF = Json.createReaderFactory(emptyMap<String, Any>())
+    }
+
+    init {
+        greeting = config["app.greeting"].asString().orElse("Ciao")
+    }
+}
---
+++ cors/src/test/java/io/helidon/kotlin/examples/cors/MainTest.kt
@@ -0,0 +1,227 @@
+/*
+ * Copyright (c) 2020 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.helidon.kotlin.examples.cors
+
+import io.helidon.common.http.Headers
+import io.helidon.common.http.MediaType
+import io.helidon.config.Config
+import io.helidon.kotlin.examples.cors.GreetingMessage.Companion.fromRest
+import io.helidon.kotlin.examples.cors.Main.startServer
+import io.helidon.media.jsonp.JsonpSupport
+import io.helidon.webclient.WebClient
+import io.helidon.webclient.WebClientRequestBuilder
+import io.helidon.webclient.WebClientResponse
+import io.helidon.webserver.WebServer
+import io.helidon.webserver.cors.CrossOriginConfig
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.TimeUnit
+import javax.json.JsonObject
+
+@TestMethodOrder(OrderAnnotation::class)
+class MainTest {
+    @Order(1) // Make sure this runs before the greeting message changes so responses are deterministic.
+    @Test
+    @Throws(Exception::class)
+    fun testHelloWorld() {
+        var r = getResponse("/greet")
+        Assertions.assertEquals(200, r.status().code(), "HTTP response1")
+        Assertions.assertEquals("Hello World!", fromPayload(r).message,
+                "default message")
+        r = getResponse("/greet/Joe")
+        Assertions.assertEquals(200, r.status().code(), "HTTP response2")
+        Assertions.assertEquals("Hello Joe!", fromPayload(r).message,
+                "hello Joe message")
+        r = putResponse("/greet/greeting", GreetingMessage("Hola"))
+        Assertions.assertEquals(204, r.status().code(), "HTTP response3")
+        r = getResponse("/greet/Jose")
+        Assertions.assertEquals(200, r.status().code(), "HTTP response4")
+        Assertions.assertEquals("Hola Jose!", fromPayload(r).message,
+                "hola Jose message")
+        r = getResponse("/health")
+        Assertions.assertEquals(200, r.status().code(), "HTTP response2")
+        r = getResponse("/metrics")
+        Assertions.assertEquals(200, r.status().code(), "HTTP response2")
+    }
+
+    @Order(10) // Run after the non-CORS tests (so the greeting is Hola) but before the CORS test that changes the greeting again.
+    @Test
+    @Throws(Exception::class)
+    fun testAnonymousGreetWithCors() {
+        val builder = webClient!!.get()
+        var headers: Headers = builder.headers()
+        headers.add("Origin", "http://foo.com")
+        headers.add("Host", "here.com")
+        val r = getResponse("/greet", builder)
+        Assertions.assertEquals(200, r.status().code(), "HTTP response")
+        val payload = fromPayload(r).message
+        Assertions.assertTrue(payload.contains("Hola World"), "HTTP response payload was $payload")
+        headers = r.headers()
+        val allowOrigin = headers.value(CrossOriginConfig.ACCESS_CONTROL_ALLOW_ORIGIN)
+        Assertions.assertTrue(allowOrigin.isPresent,
+                "Expected CORS header " + CrossOriginConfig.ACCESS_CONTROL_ALLOW_ORIGIN + " is absent")
+        Assertions.assertEquals(allowOrigin.get(), "*")
+    }
+
+    @Order(11) // Run after the non-CORS tests but before other CORS tests.
+    @Test
+    @Throws(Exception::class)
+    fun testGreetingChangeWithCors() {
+
+        // Send the pre-flight request and check the response.
+        var builder = webClient!!.options()
+        var headers: Headers = builder.headers()
+        headers.add("Origin", "http://foo.com")
+        headers.add("Host", "here.com")
+        headers.add("Access-Control-Request-Method", "PUT")
+        var r = builder.path("/greet/greeting")
+                .submit()
+                .toCompletableFuture()
+                .get()
+        val preflightResponseHeaders: Headers = r.headers()
+        val allowMethods = preflightResponseHeaders.values(CrossOriginConfig.ACCESS_CONTROL_ALLOW_METHODS)
+        Assertions.assertFalse(allowMethods.isEmpty(),
+                "pre-flight response does not include " + CrossOriginConfig.ACCESS_CONTROL_ALLOW_METHODS)
+        Assertions.assertTrue(allowMethods.contains("PUT"))
+        var allowOrigins = preflightResponseHeaders.values(CrossOriginConfig.ACCESS_CONTROL_ALLOW_ORIGIN)
+        Assertions.assertFalse(allowOrigins.isEmpty(),
+                "pre-flight response does not include " + CrossOriginConfig.ACCESS_CONTROL_ALLOW_ORIGIN)
+        Assertions.assertTrue(allowOrigins.contains("http://foo.com"), "Header " + CrossOriginConfig.ACCESS_CONTROL_ALLOW_ORIGIN
+                + " should contain '*' but does not; " + allowOrigins)
+
+        // Send the follow-up request.
+        builder = webClient!!.put()
+        headers = builder.headers()
+        headers.add("Origin", "http://foo.com")
+        headers.add("Host", "here.com")
+        headers.addAll(preflightResponseHeaders)
+        r = putResponse("/greet/greeting", GreetingMessage("Cheers"), builder)
+        Assertions.assertEquals(204, r.status().code(), "HTTP response3")
+        headers = r.headers()
+        allowOrigins = headers.values(CrossOriginConfig.ACCESS_CONTROL_ALLOW_ORIGIN)
+        Assertions.assertFalse(allowOrigins.isEmpty(),
+                "Expected CORS header " + CrossOriginConfig.ACCESS_CONTROL_ALLOW_ORIGIN + " has no value(s)")
+        Assertions.assertTrue(allowOrigins.contains("http://foo.com"), "Header " + CrossOriginConfig.ACCESS_CONTROL_ALLOW_ORIGIN
+                + " should contain '*' but does not; " + allowOrigins)
+    }
+
+    @Order(12) // Run after CORS test changes greeting to Cheers.
+    @Test
+    @Throws(Exception::class)
+    fun testNamedGreetWithCors() {
+        val builder = webClient!!.get()
+        var headers: Headers = builder.headers()
+        headers.add("Origin", "http://foo.com")
+        headers.add("Host", "here.com")
+        val r = getResponse("/greet/Maria", builder)
+        Assertions.assertEquals(200, r.status().code(), "HTTP response")
+        Assertions.assertTrue(fromPayload(r).message.contains("Cheers Maria"))
+        headers = r.headers()
+        val allowOrigin = headers.value(CrossOriginConfig.ACCESS_CONTROL_ALLOW_ORIGIN)
+        Assertions.assertTrue(allowOrigin.isPresent,
+                "Expected CORS header " + CrossOriginConfig.ACCESS_CONTROL_ALLOW_ORIGIN + " is absent")
+        Assertions.assertEquals(allowOrigin.get(), "*")
+    }
+
+    @Order(100) // After all other tests so we can rely on deterministic greetings.
+    @Test
+    @Throws(Exception::class)
+    fun testGreetingChangeWithCorsAndOtherOrigin() {
+        val builder = webClient!!.put()
+        val headers: Headers = builder.headers()
+        headers.add("Origin", "http://other.com")
+        headers.add("Host", "here.com")
+        val r = putResponse("/greet/greeting", GreetingMessage("Ahoy"), builder)
+        // Result depends on whether we are using overrides or not.
+        val isOverriding = Config.create()["cors"].exists()
+        Assertions.assertEquals(if (isOverriding) 204 else 403, r.status().code(), "HTTP response3")
+    }
+
+    companion object {
+        private var webServer: WebServer? = null
+        private var webClient: WebClient? = null
+
+        @BeforeAll
+        @JvmStatic
+        @Throws(Exception::class)
+        fun start() {
+            // the port is only available if the server started already!
+            // so we need to wait
+            webServer = startServer()
+                    .start()
+                    .toCompletableFuture()
+                    .get()
+            WebClient.builder()
+                    .baseUri("http://localhost:" + webServer!!.port())
+                    .addMediaSupport(JsonpSupport.create())
+                    .build().also { webClient = it }
+            val timeout: Long = 2000 // 2 seconds should be enough to start the server
+            val now = System.currentTimeMillis()
+            while (!webServer!!.isRunning) {
+                Thread.sleep(100)
+                if (System.currentTimeMillis() - now > timeout) {
+                    Assertions.fail<Any>("Failed to start webserver")
+                }
+            }
+        }
+
+        @AfterAll
+        @JvmStatic
+        @Throws(Exception::class)
+        fun stop() {
+            if (webServer != null) {
+                webServer!!.shutdown()
+                        .toCompletableFuture()[10, TimeUnit.SECONDS]
+            }
+        }
+
+        @Throws(ExecutionException::class, InterruptedException::class)
+        private fun getResponse(path: String): WebClientResponse {
+            return getResponse(path, webClient!!.get())
+        }
+
+        @Throws(ExecutionException::class, InterruptedException::class)
+        private fun getResponse(path: String, builder: WebClientRequestBuilder): WebClientResponse {
+            return builder
+                    .accept(MediaType.APPLICATION_JSON)
+                    .path(path)
+                    .submit()
+                    .toCompletableFuture()
+                    .get()
+        }
+
+        @Throws(ExecutionException::class, InterruptedException::class)
+        private fun putResponse(path: String, payload: GreetingMessage, builder: WebClientRequestBuilder = webClient!!.put()): WebClientResponse {
+            return builder
+                    .accept(MediaType.APPLICATION_JSON)
+                    .path(path)
+                    .submit(payload.forRest())
+                    .toCompletableFuture()
+                    .get()
+        }
+
+        @Throws(ExecutionException::class, InterruptedException::class)
+        private fun fromPayload(response: WebClientResponse): GreetingMessage {
+            val json = response
+                    .content()
+                    .`as`(JsonObject::class.java)
+                    .toCompletableFuture()
+                    .get()
+            return fromRest(json)
+        }
+    }
+}
